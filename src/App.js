6e041198f7cbc03fdf79124d8269c6ecbfd85ef7
SATEJ SARKER <satejall@gmail.com>
2018-09-22 11:46:29 +0600

    Comunication done and the project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {
   Component
 } from 'react';
-import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
+import {HashRouter as Router,Route,Switch} from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
